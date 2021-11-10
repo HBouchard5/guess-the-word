1e18e087503e046aa33c10181869cd2099134482
heidi_b <bouchard.heidi@gmail.com>
2021-11-09 22:32:56 -0500

    displays word in progress with correct letters

--- js/script.js
+++ js/script.js
@@ -41,9 +41,52 @@ const makeGuess = function(letter) {
     if (guessedLettersArray.includes(upperLetter) == true) {
         guessMessage.innerHTML = "Letter has been guessed. Try again";
     } else {
-        guessedLettersArray.push(upperLetter);
+        guessedLettersArray.push(upperLetter);  //add guess to Array
+        showGuessedLetters();       //display updated list of guessed letters
     }
     console.log(guessedLettersArray);
+    updateWIP(guessedLettersArray); //call function to update WIP
+};
+
+//function to display wrongly guessed letters
+const showGuessedLetters = function() {
+    guessedLetters.innerHTML = "";
+    for (let x of guessedLettersArray) {
+        const newLI = document.createElement("li"); //create new li for each guessed letter
+        newLI.innerHTML = x;
+        guessedLetters.appendChild(newLI);      //add each letter to unordered list to display
+    }
+};
+
+//function to update word in progress with letters to replace circles
+const updateWIP = function(guessedArray) {
+    const wordUpper = word.toUpperCase();
+    const wordArray = wordUpper.split("");
+    const mixedArray = [];      //create new array to populate with correct letters and circles
+
+    for (let x of wordArray) {
+        if (guessedArray.includes(x) == true) {
+            for (let y of guessedArray) {
+                if (y == x) {
+                    mixedArray.push(y); //update WIP with guessed letter
+                }
+            }
+        } else {
+            mixedArray.push("●"); //populate with circle placeholder
+        }
+    }
+    const mixedWord = mixedArray.join("");  //convert WIP array to string
+    wordInProgress.innerText = mixedWord;   //display WIP on screen
+    wonGame(mixedWord);  //check if the user has won the game yet
+};
+
+//function to check if the user has won the game
+const wonGame = function(WIP) {
+    const checkWord = word.toUpperCase();
+    if (WIP == checkWord) {
+        guessMessage.classList.add("win");
+        guessMessage.innerHTML = "You guessed correct the word! Congrats!";
+    } 
 };
 
 //call function to populate initial circles
